161a6e0786d789f145724f6b6ad0154e70aafe26
EnjoyTheNoise <jankos.pdp@gmail.com>
2018-04-06 14:23:54 +0200

    Rewrite console scrambler script into scrambler class, add signal class
    added simple test in main, commented call for mainView as it was't necessary now

--- main.m
+++ main.m
@@ -6,5 +6,19 @@ addpath(genpath('model'));
 %zdefiniowanej w glownyWidok.m, która z kolei odpala figure
 %glownyWidok.fig, stworzon¹ przy pomocy GUIDE (lewym na glownyWidok.fig i
 %edytuj w GUIDE)
-mainView();
+%mainView();
 
+test_sig = signal(16);
+test_sig.setBitTrue(1);
+test_sig.setBitTrue(3);
+test_sig.setBitTrue(12);
+test_sig.printSignal();     %powinno wypisaæ 1010000000010000 i tak te¿ siê dzieje
+
+scrambler = scrambler();    %obiekt scramblera
+
+copy_sig = test_sig.copy(); %kopia sygna³u zostanie zescramblowana
+
+scrambler.scramble_signal(copy_sig);    %scramblowanie copy_sig
+
+fprintf("\ntest_sig after scrambling:\n");   
+copy_sig.printSignal();     %wydruk po scramblingu
--- model/scrambler.m
+++ model/scrambler.m
@@ -1,24 +1,44 @@
-clear;
-
-signal = [0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,];
-frame  = [0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1];
-output = zeros(32);
-
-fprintf("Przed: [");
-for i = 1 : length(signal)
-    fprintf("%d ", signal(i));
+classdef scrambler < handle
+    
+    properties (Access = private)
+        default_seed
+        lfs_register
+    end
+    
+    methods
+        function this = scrambler(seed) %konstruktor
+            if(nargin == 0)             %number of function input arguments = 0 -> konstruktor bezparametryczny
+                this.default_seed = [0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1];
+                this.lfs_register = this.default_seed;
+            else                        %seed z parametru
+                this.default_seed = seed;
+                this.lfs_register = seed;
+            end
+        end
+        
+        function reset_register(this)   %reset rejestru do stanu pocz¹tkowego
+            this.lfs_register = this.default_seed;
+        end
+        
+        function scr = scramble_signal(this, signal)    %funkcja scrambluj¹ca
+            for i = 1:signal.getSize()  
+                x = xor(this.lfs_register(1), xor(this.lfs_register(21), this.lfs_register(37))); %xor 1,21 i 37 indeksu lfsr
+                x = xor(signal.getBitAt(i), x);     %xor danego bitu sygna³u oraz wartoœci z poprzedniego xora
+                signal.setBitAt(i, x);              %ustawienie bitu sygna³u wyjœciowego
+                
+                this.lfs_register = [signal.getBitAt(i), this.lfs_register(1:end-1)];  %przesuniêcie rejestru
+                
+                scr = signal;       %return signal
+            end
+        end
+        
+        function print_register(this)   %wydruk stanu rejestru lfs
+            fprintf("Rejestr generatora:\n[");
+            for i = 1 : size(this.lfs_register,2)
+                fprintf("%d, ", this.lfs_register(i));
+            end
+            fprintf(" ]\n");
+        end
+        
+    end
 end
-fprintf("\b]\n");
-
-for i = 1 : length(signal)
-    x = xor(frame(1), xor(frame(40), frame(59)));   %xor bitu 1, 40, 59
-    x = xor(signal(i), x);                          %xor bitu wejœcia i wartoœci pseudolosowej
-    output(i) = x;                                  %dla czytelnoœci w osobny sygna³
-    frame = [output(i), frame(1:end-1)];            %nowa wartoœæ do ramki, przesuniêcie reszty
-end
-
-fprintf("Po:    [");
-for i = 1 : length(output)
-    fprintf("%d ", output(i));
-end
-fprintf("\b]\n");
