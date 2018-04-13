789f74d556095f575265bd552808a4cc9c66b089
Adamos45 <37145552+Adamos45@users.noreply.github.com>
2018-04-13 03:25:15 +0200

    poprawa komentarzy

--- model/koder_script.m
+++ model/koder_script.m
@@ -1,12 +1,12 @@
 clc;
 
-esignal=randi([0,1],1,127); %generuje stu-bitowy sygna³ w zakresie od 0 do 1. Normalnie bêdzie tu wchodzi³ ca³y wygenerowany sygna³
+esignal=randi([0,1],1,127); %generuje 127-bitowy sygna³ w zakresie od 0 do 1. Normalnie bêdzie tu wchodzi³ ca³y wygenerowany sygna³
 preambule = [0 1];
 
 disp(esignal); % wyswietlenie sygnalu przed kodowaniem
 temp=1;
 org_signal_size = length(esignal);
-newesignal=[]; %bedzie zbierala 66 bitowymi kawa³kami preambu³e+sygna³ wejsciowy
+newesignal=[]; %bedzie zbierala 66 bitowymi ramkami preambu³e+sygna³ wejsciowy
 
 while(temp<org_signal_size)
     if(temp+64>org_signal_size)
