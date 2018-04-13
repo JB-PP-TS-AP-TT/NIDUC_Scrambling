697171f645d9a81acd67f0079f620b67276345b5
Adamos45 <37145552+Adamos45@users.noreply.github.com>
2018-04-13 02:17:15 +0200

    koder_script++
    poprawa

--- model/koder_script.m
+++ model/koder_script.m
@@ -1,6 +1,6 @@
 clc;
 
-esignal=randi([0,1],1,100); %generuje stu-bitowy sygna³ w zakresie od 0 do 1. Normalnie bêdzie tu wchodzi³ ca³y wygenerowany sygna³
+esignal=randi([0,1],1,127); %generuje stu-bitowy sygna³ w zakresie od 0 do 1. Normalnie bêdzie tu wchodzi³ ca³y wygenerowany sygna³
 preambule = [0 1];
 
 disp(esignal); % wyswietlenie sygnalu przed kodowaniem
@@ -9,6 +9,10 @@ org_signal_size = length(esignal);
 newesignal=[]; %bedzie zbierala 66 bitowymi kawa³kami preambu³e+sygna³ wejsciowy
 
 while(temp<org_signal_size)
+    if(temp+64>org_signal_size)
+        newesignal=horzcat(newesignal,horzcat(preambule,esignal(temp:org_signal_size)));
+        break;
+    end
     newesignal=horzcat(newesignal,horzcat(preambule,esignal(temp:temp+63))); 
     temp = temp + 64;
     if(temp+64>org_signal_size)
