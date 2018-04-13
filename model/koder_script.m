119c49c42b65dd1e85dfc1d1c7e2ef7a463d62ae
Adamos45 <37145552+Adamos45@users.noreply.github.com>
2018-04-13 02:10:58 +0200

    koder_skript
    koder_skript

---
+++ model/koder_script.m
@@ -0,0 +1,19 @@
+clc;
+
+esignal=randi([0,1],1,100); %generuje stu-bitowy sygna³ w zakresie od 0 do 1. Normalnie bêdzie tu wchodzi³ ca³y wygenerowany sygna³
+preambule = [0 1];
+
+disp(esignal); % wyswietlenie sygnalu przed kodowaniem
+temp=1;
+org_signal_size = length(esignal);
+newesignal=[]; %bedzie zbierala 66 bitowymi kawa³kami preambu³e+sygna³ wejsciowy
+
+while(temp<org_signal_size)
+    newesignal=horzcat(newesignal,horzcat(preambule,esignal(temp:temp+63))); 
+    temp = temp + 64;
+    if(temp+64>org_signal_size)
+        newesignal=horzcat(newesignal,horzcat(preambule,esignal(temp:org_signal_size)));
+        break;
+    end
+end
+disp(newesignal); % sprawdzenie po kodowaniu
