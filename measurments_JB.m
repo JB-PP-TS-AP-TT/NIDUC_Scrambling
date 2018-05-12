1116a758b3f16bfd76a219cd2547f4eddd474bdb
Jot_Be <jakubalcerzak@gmail.com>
2018-05-12 19:36:48 +0200

    Measurments_JB
    ->I've just did all time measurments

--- measurments_JB.m
+++ measurments_JB.m
@@ -7,12 +7,12 @@ addpath(genpath('helper'));
 %1)CC (bsc p=0.02) (j=0.5) (desync = 18 albo tyle ile nakazuje norma)
 %2)CC (bsc p=0.02) (j=0.8) (desync = 18 albo tyle ile nakazuje norma)
 %3)CC (bsc p=0.00) (j=0.7) (desync = 18 albo tyle ile nakazuje norma)
-G = SignalGenerator(10, 0.7);     %generator sygna³u 
+G = SignalGenerator(200, 0.8);  %generator sygna³u 
 S = Scrambler();                %scrambler
 D = Descrambler();              %descrambler
 Enc = Encoder();                %koder Ethernet
 Dec = Decoder2();               %dekoder Ethernet
-CC = CustomChannel(0,18,0);  %bsc = 0.02, desynch = 18, period=0
+CC = CustomChannel(0.02,18,0);  %bsc = 0, desynch = 18, period=0
 H = Helper();                   %obliczanie BER
 %zmienne sygna³ oraz BER
 sig = G.generateSignal();
@@ -20,11 +20,13 @@ BER = 0;
 for i=1 : 500
     cop = sig.copy();
     cop = S.scrambleSignal(cop);
-    %cop = Enc.encode(cop);
+    cop = Enc.encode(cop);
     CC.sendSig(cop);
     cop = CC.receiveSig();
-    %cop = Dec.decode(cop);
+    cop = Dec.decode(cop);
     cop = D.descrambleSignal(cop);
     BER = BER + H.calculateBER(sig,cop);
+    S.resetLSFR();
+    D.resetLSFR();
 end
 BER = BER / 500;
